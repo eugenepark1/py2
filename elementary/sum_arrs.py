8bd0d37d9ca17496ad150646cf96b0c4c0f0dfa4
eugenep <eugenep@EUGENEP01-PC.hq.netapp.com>
2017-09-24 05:14:36 -0400

    commit

---
+++ elementary/sum_arrs.py
@@ -0,0 +1,21 @@
+'''
+Created on Sep 24, 2017
+
+@author: eugenep
+'''
+
+
+
+def sum_arrs(first, second):
+    f_len = len(first)
+    s_len = len(second)
+    
+    return
+
+
+
+
+first = [1,2,3]
+second = [8,7]
+
+print sum_arrs(first, second)
