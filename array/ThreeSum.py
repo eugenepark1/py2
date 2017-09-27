c2200e41b8ebc8da3ba4fcb75b6cee95eb5bbfe2
eugenep <eugenep@EUGENEP01-PC.hq.netapp.com>
2017-09-27 14:00:21 -0400

    str commit

---
+++ array/ThreeSum.py
@@ -0,0 +1,17 @@
+'''
+Created on Sep 27, 2017
+
+@author: eugenep
+
+given an array of S of n integers, are there elements, a, b, c in S such that a + b + c = 0?
+Fine all unique triplets in the array which gives the sum of zero
+
+Note: solution must not contain duplicate triplets
+
+given [-1, 0, 1, 2, -1, -4]
+
+solution is [
+    [-1, 0, 1]
+    [-1, -1, 2]
+]
+'''
---
+++ string/Longest_substr_wo_repeating_chars.py
@@ -0,0 +1,11 @@
+'''
+Created on Sep 27, 2017
+
+@author: eugenep
+
+given a string, find the length of the longest substring w/o repeating chars
+
+given 'abcabcb' -> 'abc' 3
+given 'bbbbbb' -> 'b' 1
+given 'pwwkew' -> 'wke' 3
+'''
