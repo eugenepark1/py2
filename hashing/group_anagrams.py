afa70c841396a92e09cef7f7c2319db9bbc58259
eugenep <eugenep@EUGENEP01-PC.hq.netapp.com>
2017-10-02 10:20:23 -0400

    group_ana

--- hashing/group_anagrams.py
+++ hashing/group_anagrams.py
@@ -17,12 +17,37 @@ return [
 '''
 import unittest
 
+def group_anagrams(arr):
+
+    group_dict = {}
+    for word in arr:
+        hashcode = str(sorted(word))
+        if hashcode not in group_dict:
+            group_dict[hashcode] = []
+        group_dict[hashcode].append(word)
+        
+    group_list = [sorted(v) for k,v in group_dict.iteritems()]
+
+    return group_list
 
 class Test(unittest.TestCase):
 
 
     def testName(self):
         pass
+    
+    def test_anagram(self):
+        arr = ["eat", "tea", "tan", "ate", "nat", "bat"]
+
+        expected = [
+                ["ate", "eat", "tea"],
+                ["nat", "tan"],
+                ["bat"]
+            ]
+        
+        actual = group_anagrams(arr)
+
+        self.assertItemsEqual(expected, actual, "")
 
 
 if __name__ == "__main__":
