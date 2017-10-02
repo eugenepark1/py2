b16a2fe3974dd3fc7eacbb52fe5a7281bf699fc6
eugen <eugen@DESKTOP-09040E9>
2017-10-02 08:27:29 -0400

    group ana

---
+++ hashing/group_anagrams.py
@@ -0,0 +1,30 @@
+'''
+Created on Oct 1, 2017
+
+@author: eugene
+
+given an array of strings, group anagrams together
+
+anagram: a word, phrase, or sentence formed from another by rearranging its letters
+given ["eat", "tea", "tan", "ate", "nat", "bat"]
+
+return [
+    ["ate", "eat", "tea"],
+    ["nat", "tan"],
+    ["bat"]
+]
+
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
