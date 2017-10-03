1f23722308ad846036674e68700d00899fa94771
eugenep <eugenep@EUGENEP01-PC.hq.netapp.com>
2017-10-03 12:33:43 -0400

    kth largest

---
+++ heap/kth_largest_element.py
@@ -0,0 +1,40 @@
+'''
+Created on Oct 2, 2017
+
+@author: eugenep
+
+1) sort the array - O(N lg N)
+2) min-heap O(N log k)
+3) selection algo based on the partition methoed - the samed used for quicksort
+    O(N) best O(N^2) worst
+    randomized input to guarantee O(n)
+'''
+import unittest
+import heapq
+
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+
+    def test_kth_largest_heap(self):
+        arr = [5,7,3,2,9]
+        
+        heapq.heapify(arr)
+        
+        k = 3
+        k_popped = []
+        for i in range(3):
+            k_popped.append(heapq.heappop(arr))
+        
+        expected = [2,3,5]
+        self.assertItemsEqual(expected, k_popped, "expected: %s actual: %s" % (expected, k_popped))
+        
+        return
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
