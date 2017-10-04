b6ef6bd257e6d20bde7ff55e93c2ef6043e19b37
eugenep <eugenep@EUGENEP01-PC.hq.netapp.com>
2017-10-04 12:15:55 -0400

    find k pairs w smallest sums

--- heap/find_k_pairs_w_smallest_sums.py
+++ heap/find_k_pairs_w_smallest_sums.py
@@ -9,8 +9,19 @@ define a pair(u,v) which consists of one element from the first and one element 
 
 find k pairs w/ the smallest sums
 (u1, v1), (u2, v2),...,(uk,vk) with the smallest sums
+
+nums1 [1,7,11]
+nums2 [2,4,6]
+k=3
+return [1,2], [1,4], [1,6]
+
+nums1 [1,1,2]
+nums2[2,4,6]
+k=2
+return [1,1], [1,1]
 '''
 import unittest
+from heapq import heappush,heappop
 
 
 class Test(unittest.TestCase):
@@ -18,6 +29,34 @@ class Test(unittest.TestCase):
 
     def testName(self):
         pass
+    
+    def test_sum_two_arrs(self):
+        nums1 = [1,7,11]
+        nums2 = [2,4,6]
+        
+        # 1, 2
+        # 1, 4
+        # 1, 6
+        # 7, 2
+        # 7, 4
+        # 7, 6
+        #...
+        
+        k = 3
+        h = []
+        for i, ele1 in enumerate(nums1):
+            for j, ele2 in enumerate(nums2):
+                sum = ele1 + ele2
+                heappush(h, (ele1+ele2, (ele1, ele2)))
+        
+        res = []
+        for i in range(k):
+            res.append(heappop(h)[1])
+            
+        expected = [(1,2), (1,4), (1,6)]
+        
+        self.assertItemsEqual(expected, res, "")
+        
 
 
 if __name__ == "__main__":
