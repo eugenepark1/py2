b74f7985d7947b49a9be3653b3217caef0143191
eugenepark1 <eugene@everydayrelay.com>
2017-10-07 21:28:49 -0400

    template for longest palindromic substring

---
+++ string/longest_palindromic_substring.py
@@ -0,0 +1,28 @@
+'''
+Created on Oct 7, 2017
+
+@author: eugenep
+
+given a string s, find the longest palindromic substring in s
+
+input babad
+output bab or aba
+
+input cbbd
+output bb
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        
+        
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
