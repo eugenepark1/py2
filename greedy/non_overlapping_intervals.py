a98289fec89a80fb3d978bad966fe1d0427cbd16
eugenepark1 <eugene@everydayrelay.com>
2017-10-09 04:00:52 -0400

    oct 9

---
+++ greedy/non_overlapping_intervals.py
@@ -0,0 +1,11 @@
+'''
+Created on Oct 9, 2017
+
+@author: eugene
+
+given a collection of intervals, find the min number of intervals you need to remove to make the rest of the interval non-overlapping
+[ [1,2], [2,3], [3,4], [1,3] ]
+output 1  [1,3] should be removed
+
+classic greedy problem : interval scheduling
+'''
