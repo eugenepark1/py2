60437ec126a4a0992998ad4fdaf954162832dfb8
eugenepark1 <eugene@everydayrelay.com>
2017-10-10 01:49:51 -0400

    oct 10

--- interview/cool_string.py
+++ interview/cool_string.py
@@ -6,3 +6,18 @@ Created on Oct 9, 2017
 a cool string is a string where each char has the name number of counts
 or where one and only one can be removed  
 '''
+
+
+def cool_string(input_str):
+    
+    countMap = {}
+    for ch in input_str:
+        if not ch in countMap:
+            countMap[ch] = 0
+        countMap[ch] += 1
+    
+    freqMap = {}
+    for ch, cnt in countMap.iteritems():
+        if cnt not in freqMap:
+            freqMap[cnt] = []
+        freqMap[cnt].append(ch)
