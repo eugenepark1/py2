02bc11bbc6f60378e52704172348b0eaeaf103f5
eugenepark1 <eugene@everydayrelay.com>
2017-10-12 23:28:15 -0400

    balanced_paranthesis

--- interview/balanced_parens.py
+++ interview/balanced_parens.py
@@ -10,6 +10,34 @@ Created on Oct 9, 2017
 returns true of a string has balanced ([{}])   
 '''
 
+stacks = {
+        '[' : [],
+        '(' : [],
+        '{' : []
+    }
 
+closedMap = {
+        ']' : '[',
+        ')' : '(',
+        '}' : '{',
+    }
+
+opens = [i for i in stacks.iterkeys()]
 def is_balanced(input_str):
-    return
+    for ch in input_str:
+        if ch in opens:
+            stacks[ch].append(ch)
+        else: # closed
+            open_counterpart = closedMap[ch]
+            if len(stacks[open_counterpart]) == 0:
+                return False
+            else:
+                popped = stacks[open_counterpart].pop()
+                if popped != open_counterpart:
+                    return False 
+    return True
+            
+assert is_balanced("()") is True
+assert is_balanced("())") is False
+assert is_balanced("((()))") is True
+assert is_balanced(")(") is False
