6b9619bc602eaabf12146f8e015c631f0ac68351
eugenepark1 <eugene@everydayrelay.com>
2017-10-15 18:44:44 -0400

    non overlapping

---
+++ array/non_overlapping_intervals.py
@@ -0,0 +1,12 @@
+'''
+Created on Oct 15, 2017
+
+@author: eugenep
+
+given a collection of intervals, 
+find the minimum number of intervals you need to remove to make the rest of the intervals non-overlaping
+
+arr = [ [1,2], [2,3], [3,4], [1,3] ] 
+output = 1
+
+'''
