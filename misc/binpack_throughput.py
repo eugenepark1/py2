bc4683ce92425d8ec93d97cd57dc997947692a5f
eugenepark1 <eugene@everydayrelay.com>
2017-10-16 15:50:18 -0400

    binpack throughput

--- misc/binpack_throughput.py
+++ misc/binpack_throughput.py
@@ -7,32 +7,12 @@ given a throughput (throughput meaning, how many jobs which can run in parallel,
 to elaborate, even if you have a throughput of 1000 jobs/sec, if there are 10 cores, then obv not all 1000 jobs will run in parallel
 in this problem, we will assume that there are enough resources to match throughput 
 
-
-2017-10-16 12:16:07,148 - __main__ - INFO - 153: 1
-2017-10-16 12:16:07,148 - __main__ - INFO - 31: 1
-2017-10-16 12:16:07,148 - __main__ - INFO - 25: 1
-2017-10-16 12:16:07,148 - __main__ - INFO - 24: 1
-2017-10-16 12:16:07,148 - __main__ - INFO - 17: 2
-2017-10-16 12:16:07,148 - __main__ - INFO - 16: 1
-2017-10-16 12:16:07,148 - __main__ - INFO - 15: 1
-2017-10-16 12:16:07,148 - __main__ - INFO - 14: 1
-2017-10-16 12:16:07,148 - __main__ - INFO - 13: 2
-2017-10-16 12:16:07,149 - __main__ - INFO - 12: 4
-2017-10-16 12:16:07,149 - __main__ - INFO - 11: 5
-2017-10-16 12:16:07,149 - __main__ - INFO - 10: 2
-2017-10-16 12:16:07,149 - __main__ - INFO - 9: 9
-2017-10-16 12:16:07,149 - __main__ - INFO - 8: 13
-2017-10-16 12:16:07,149 - __main__ - INFO - 7: 32
-2017-10-16 12:16:07,149 - __main__ - INFO - 6: 41
-2017-10-16 12:16:07,149 - __main__ - INFO - 5: 108
-2017-10-16 12:16:07,149 - __main__ - INFO - 4: 254
-2017-10-16 12:16:07,149 - __main__ - INFO - 3: 502
-2017-10-16 12:16:07,149 - __main__ - INFO - 2: 1100
-2017-10-16 12:16:07,149 - __main__ - INFO - 1: 790
-2017-10-16 12:16:07,149 - __main__ - INFO - 0: 2099
+find the minimum throughput for minimum elapsed time
+    implies in what order job needs to be dispatched - always sorted
 '''
 import random
 
+# klibo@ontap
 freqMap = {
     153: 1,
     31: 1,
@@ -66,9 +46,10 @@ for k,v in freqMap.iteritems():
         compile_times += [k]*v
 print compile_times
 
-compile_times = [5,1,1,1,1]
-compile_times.reverse()
-tp = 1
+#compile_times = [5,1,1,1,1]
+#compile_times.reverse()
+tp = 48
+max_parallel = 12*4
 
 running = []
 elapsed_time = 0
@@ -81,18 +62,27 @@ while True:
         if i > 0:
             tmp_running.append(i)
     running = tmp_running
-    
+
     # dispatch jobs
-    if len(compile_times) > 0:
-        for i in range(tp):
+    cnt = 0
+    while True:
+        if len(running) == max_parallel:
+            break
+
+        if len(compile_times) > 0:
             running.append( compile_times.pop(0) )
+            cnt += 1
+            if cnt == tp:
+                break
+        else:
+            break
 
     elapsed_time += 1
     
     print "remaing jobs: %s" % compile_times
     print "running jobs: %s" % running
     print "elapsed time %s"  % elapsed_time
-    
+
     if len(running) == 0:
         break
 
