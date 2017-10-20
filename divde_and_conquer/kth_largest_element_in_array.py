00b5643335c1912e7668780de74d7245ddefafc6
eugenepark1 <eugene@everydayrelay.com>
2017-10-20 15:15:33 -0400

    oct 20

---
+++ divde_and_conquer/kth_largest_element_in_array.py
@@ -0,0 +1,16 @@
+'''
+Created on Oct 8, 2017
+
+@author: eugene
+
+find the kth largest element in an unsorted array
+kth largest in sorted order
+
+arr = [3,2,1,5,6,4] k=2, returns 5
+
+selection algo based on the partition method, same as quicksort()
+'''
+
+
+def findKthLargest(arr):
+    return
