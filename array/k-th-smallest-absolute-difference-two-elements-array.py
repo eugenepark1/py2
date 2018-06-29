5ba327179d4e964748729d4303ee56d9f6b19244
eugenepark1 <eugene@everydayrelay.com>
2018-06-29 12:39:16 -0400

    added interview

---
+++ array/k-th-smallest-absolute-difference-two-elements-array.py
@@ -0,0 +1,18 @@
+'''
+Created on Jun 29, 2018
+
+@author: eugenep
+
+
+Input  : a[] = {1, 2, 3, 4}
+         k = 3
+Output : 1
+The possible absolute differences are :
+{1, 2, 3, 1, 2, 1}.
+The 3rd smallest value among these is 1.
+
+Input : n = 2
+        a[] = {10, 10}
+        k = 1
+Output : 0
+'''
