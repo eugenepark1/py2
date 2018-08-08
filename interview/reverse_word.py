717b0264129d80933cbce6aa7a2281063d37a8eb
eugenepark1 <eugene@everydayrelay.com>
2018-08-08 01:08:48 -0400

    reverse_word

---
+++ interview/reverse_word.py
@@ -0,0 +1,21 @@
+'''
+Created on Aug 8, 2018
+
+@author: eugene
+
+
+Input: "the sky is blue",
+Output: "blue is sky the".
+
+A word is defined as a sequence of non-space characters.
+Input string may contain leading or trailing spaces. However, your reversed string should not contain leading or trailing spaces.
+You need to reduce multiple spaces between two words to a single space in the reversed string.
+'''
+
+
+
+def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
