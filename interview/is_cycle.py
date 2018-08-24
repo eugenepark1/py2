dd1d4557948e2131eec25079b887845c01e904fe
eugenepark1 <eugene@everydayrelay.com>
2018-08-24 02:41:25 -0400

    added another test case

--- interview/is_cycle.py
+++ interview/is_cycle.py
@@ -9,6 +9,13 @@ returns True if:
     1. start == end
     2. every index is visited
     3. only wraps around once
+    
+    
+    
+step   pos     visited     new_pos
+0       0        [0]         2
+1       2       [0,2]        1
+2       1       [0,1,2]      0
 '''
 
 
@@ -17,17 +24,21 @@ def is_cycle(arr):
     visited = set()
     pos = 0
     for step in range(len(arr)):
-        pos = (pos + arr[pos]) % len(arr)
+
         if pos in visited:
             return False
         else: 
             visited.add(pos)
+            
+        pos = (pos + arr[pos]) % len(arr)
+
+        if pos == 0 and len(visited) < len(arr):
+            return False
 
     return pos == 0
     
 
 
-circular_array = [2, 2, -1]
 
 '''
 (0) (1) (2)
@@ -37,4 +48,10 @@ circular_array = [2, 2, -1]
     every index is visited
     only wraps around once
 '''
-print is_cycle(circular_array)
+circular_array = [2, -1, 2]
+print is_cycle(circular_array) is True
+
+
+
+circular_array = [3, -1, 2]
+print is_cycle(circular_array) is False
