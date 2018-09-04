4e567912c55c97022c36db0f7b6d3212e14bf7f8
eugenepark1 <eugene@everydayrelay.com>
2018-09-04 01:33:31 -0400

    guess game

---
+++ interview/google/guess_the_number_game.py
@@ -0,0 +1,25 @@
+'''
+Created on Sep 4, 2018
+
+@author: eugene
+
+from number(a,b), guess a number
+each guess costs the same amount of the guess; minimize the cost
+
+1,5
+
+
+    (1)
+2 3 4 5
+
+   (2)
+
+
+    (3)
+12      4 (5)
+
+
+
+
+      
+'''
