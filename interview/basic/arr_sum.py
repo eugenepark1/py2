033738d3b5f21c977c98ed32eeb061952cb02a6b
eugenepark1 <eugene@everydayrelay.com>
2018-09-07 15:42:33 -0400

    9 7 commit

--- interview/basic/arr_sum.py
+++ interview/basic/arr_sum.py
@@ -3,3 +3,6 @@ Created on Sep 7, 2018
 
 @author: eugenep
 '''
+
+numOne = 123
+numTwo = 3456
---
+++ interview/helper_space/n_to_end_LL.py
@@ -0,0 +1,10 @@
+'''
+Created on Sep 7, 2018
+
+@author: eugenep
+
+using extra ptr ie space improve from O(n^2) to O(n)
+
+'''
+
+
