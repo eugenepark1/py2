6ef6f7420d7e36f2310ce8c4fe3162c16716a646
eugenepark1 <eugene@everydayrelay.com>
2018-09-09 12:29:16 -0400

    palin

--- hashmap/palin.py
+++ hashmap/palin.py
@@ -30,3 +30,30 @@ def give_possible_palins(word):
 #   for possible in give_possible_palins(word)
 #       if possible in english_dict:
 #           add possible
+
+def check_palin(word):
+    for i in xrange(len(word)/2):
+        if word[i] != word[-1*(i+1)]:
+            return False
+    return True
+
+def all_palindromes(string):
+
+    left,right=0,len(string)
+    j=right
+    results=[]
+
+    while left < right-1:
+        temp = string[left:j] #Time complexity O(k)
+        j-=1
+
+        if check_palin(temp):
+            results.append(temp)
+
+        if j<left+2:
+            left+=1
+            j=right
+
+    return list(set(results))
+
+print all_palindromes("racecarenterelephantmalayalam")
