5d81d4720544f79b262559fe078bbb8b0543672d
eugenepark1 <eugene@everydayrelay.com>
2018-09-10 00:42:38 -0400

    find pairs

---
+++ interview/sum_equal_to_k.py
@@ -0,0 +1,13 @@
+'''
+Created on Sep 10, 2018
+
+@author: eugenep
+
+Given a list of numbers can you find all the pairs of numbers whose sum equals k?
+
+For example, when given the list 1 9 11 13 2 3 7 5 and k as 12, you should find these pairs: {1 11}, {9 3} and {7, 5}.
+'''
+
+
+def find_pairs(k, arr):
+    pairs = []
