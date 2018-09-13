3b94dbfc81b29a5f133ea43bea00545e5d8fd72a
eugenepark1 <eugene@everydayrelay.com>
2018-09-13 16:48:53 -0400

    find all palins

--- interview/find_all_palins.py
+++ interview/find_all_palins.py
@@ -9,8 +9,28 @@ def is_palin(word):
     mid = len(word)/2
     for i in range(mid):
         if word[i] != word[len(word)-1-i]:
-            result = False 
+            result = False
+            break
         
     return result
 
 assert is_palin("aabaa") is True
+
+
+# find all palins in a word
+
+def find_all_palins(word):
+    found_palins = []
+    for i in range(len(word)):
+        for j in range(len(word), -1, -1):
+            if is_palin(word[i:j]):
+                found_palins.append(word[i:j])
+    
+    return found_palins
+
+print find_all_palins('abababa')
+# O(n)
+
+
+#find longest palindromic substring 
+# easy solution: find all and get max
