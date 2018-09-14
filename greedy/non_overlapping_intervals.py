b1687e13173545364e69ea5cab83f069261e0c99
eugenepark1 <eugene@everydayrelay.com>
2018-09-14 13:31:49 -0400

    nonoverlap

--- greedy/non_overlapping_intervals.py
+++ greedy/non_overlapping_intervals.py
@@ -8,4 +8,37 @@ given a collection of intervals, find the min number of intervals you need to re
 output 1  [1,3] should be removed
 
 classic greedy problem : interval scheduling
+
+
 '''
+
+
+intervals = [(1,2), (2,3), (3,4), (1,3)]
+def nonoverlap(intervals):
+    '''
+    overlapping:
+        s is between (s,e)
+        e is between (s,e)
+        
+    if cur_intv nooverlapping in tmp_intvs:
+        add to tmp_intvs
+    '''
+    
+    tmp_intvs = []
+    for cur_intv in intervals:
+        c_start, c_end = cur_intv
+        
+        overlap = False
+        for tmp_intv in tmp_intvs:
+            t_start, t_end = tmp_intv
+            if (c_start > t_start and c_start < t_end) or (c_end > t_start and c_end < t_end):
+                overlap = True
+                break
+        if not overlap:
+            tmp_intvs.append(cur_intv)
+        
+        
+    return (len(intervals) - len(tmp_intvs), tmp_intvs)
+
+
+print nonoverlap(intervals)
