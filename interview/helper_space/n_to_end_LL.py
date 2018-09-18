5204f8a81f5a87a1bf8e648b4bf98d5b259c7a53
eugenepark1 <eugene@everydayrelay.com>
2018-09-18 11:15:00 -0400

    updated

--- interview/helper_space/n_to_end_LL.py
+++ interview/helper_space/n_to_end_LL.py
@@ -8,3 +8,59 @@ using extra ptr ie space improve from O(n^2) to O(n)
 '''
 
 
+class LL:
+    def __init__(self, root):
+        self.root = root
+
+    def traverse(self):
+        traverse_str = ""
+        cur = self.root
+        while cur is not None:
+            if cur.next is None:
+                traverse_str += "%s" % cur
+            else:
+                traverse_str += "%s->" % cur
+            cur = cur.next
+        print traverse_str
+    
+    def reverse(self):
+        cur = self.root
+        prev = None
+        while cur is not None:
+            tmp = cur.next
+            cur.next = prev
+            
+            # prep for next iteration
+            prev = cur
+            cur = tmp
+        self.root = prev
+        
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+    
+    def __str__(self):
+        return str(self.data)
+        
+
+n_one = Node(1)
+n_two = Node(2)
+n_three = Node(3)
+n_four = Node(4)
+
+my_LL = LL(n_one)
+n_one.next = n_two
+n_two.next = n_three
+n_three.next = n_four
+
+def n_to_the_end(n, root):
+    cur = root
+    nth_node = None
+    tmp_cnt = 0
+    while cur is not None:
+        
+        cur = cur.next
+    
+    return nth_node
+
---
+++ interview/merge_intervals_price.py
@@ -0,0 +1,12 @@
+'''
+Created on Sep 5, 2018
+
+@author: eugenep
+
+
+given a list of tuples [(start,end,price), ... ]
+output a list with lowest_prices of intervals
+
+input: [(1,8,5), (3,10,2)]
+output: [(1,3,5), (3,10,2)]
+'''
