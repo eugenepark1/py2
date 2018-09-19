97c3166d41771f1c6d6d0dcabfd3caf9a88b212d
eugenepark1 <eugene@everydayrelay.com>
2018-09-19 18:39:27 -0400

    sep 19 commit

--- interview/amzn/leaf_on_fire.py
+++ interview/amzn/leaf_on_fire.py
@@ -41,27 +41,17 @@ def get_path(root, node):
     
     
     if root is None:
-        print "root is None"
         return {'path': [],
                 'found': False
             }
     
     elif root.value == node.value:
-        print "root is equal to value (%s %s)" % (root.value, node.value)
-        
-        print "found %s" % root.value
         return {'path': [node.value],
                 'found': True
                 }
     else:
-        print "else: %s" % root.value
-        
         left = get_path(root.left, node)
         right = get_path(root.right, node)
-        
-        print "left: %s (%s)" % (left, root.value)
-        print "right: %s (%s)" % (right, root.value)
-    
         
         if left['found']:
             return {'path': [root.value] + left['path'],
@@ -77,33 +67,59 @@ def get_path(root, node):
             }
  
  
-print get_path(oneNode, fourNode_right_left_left)
-    
 def gather_nodes(root):
     if root is None:
         return []
     else:
-        return [root.value] + gather_nodes(root.left) + gather_nodes(root.right)
+        return [root] + gather_nodes(root.left) + gather_nodes(root.right)
 
-def calculate_time_to_burn(root, fireLeaf):
-    t = 0
+notVisitedNodes = []
     
+def calculate_time_to_burn(root, fireLeaf):
+    global notVisitedNodes
+    t = 0
     notVisitedNodes = gather_nodes(root)
+    
     latest_fire_nodes = [fireLeaf]
     while len(notVisitedNodes) > 0:
         
+        
+        tmp_latest_fire_nodes = []
         # step
         for f_node in latest_fire_nodes:
-            notVisitedNodes.remove(f_node.value)
+            notVisitedNodes.remove(f_node)
 
             # add parent, childrent to latest_fire_nodes
             f_node_path = get_path(root, f_node)
-            
+            if len(f_node_path) > 1:
+                parent = f_node_path['path'] = -2
+                
+                # add parent to latest firenode
+                for node in notVisitedNodes:
+                    if node.value == parent:
+                        tmp_latest_fire_nodes.append(node)
+                
+            # add children to latest fire nodes
+            for node in notVisitedNodes:
+                try:
+                    if node.value == f_node.left.value:
+                        tmp_latest_fire_nodes.append(f_node.left)
+                except:
+                    pass
+            for node in notVisitedNodes:
+                try:
+                    if node.value == f_node.right.value:
+                        tmp_latest_fire_nodes.append(f_node.right)
+                except:
+                    pass
+        latest_fire_nodes = tmp_latest_fire_nodes
             
         t += 1
+        print t
     
     return t
     
-
-#calculate_time_to_burn(oneNode, threeNode_right_right)
+print get_path(oneNode, fourNode_right_left_left)
+print [node.value for node in gather_nodes(oneNode)]
+print calculate_time_to_burn(oneNode, threeNode_right_right)
     
