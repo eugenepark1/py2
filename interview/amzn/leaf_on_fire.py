08a581b03218124d3270275fb76294e52648c080
eugenepark1 <eugene@everydayrelay.com>
2018-09-19 08:19:19 -0400

    sep 19 commit

--- interview/amzn/leaf_on_fire.py
+++ interview/amzn/leaf_on_fire.py
@@ -3,3 +3,97 @@ Created on Sep 18, 2018
 
 @author: eugenep
 '''
+
+class Node:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+        
+'''
+              4
+       5              8
+    7    10       12    3
+                9
+'''
+
+oneNode = Node(4)
+twoNode_left = Node(5)
+twoNode_right = Node(8)
+oneNode.left = twoNode_left
+oneNode.right = twoNode_right
+
+threeNode_left_left = Node(7)
+threeNode_left_right = Node(10)
+twoNode_left.left = threeNode_left_left
+twoNode_left.right = threeNode_left_right
+
+threeNode_right_left = Node(12)
+threeNode_right_right = Node(3)
+twoNode_right.left = threeNode_right_left
+twoNode_right.right = threeNode_right_right
+
+fourNode_right_left_left = Node(9)
+threeNode_right_left.left = fourNode_right_left_left
+
+
+def get_path(root, node):
+    
+    if root is None:
+        return {'path': [],
+                'found': False
+            }
+    
+    if root.value == node.value:
+        print "found %s" % root.value
+        return {'path': [node.value],
+                'found': True
+                }
+    else:
+        left = get_path(root.left, node)
+        right = get_path(root.right, node)
+        
+        print "%s, left:%s right: %s (%s)" % (root.value, root.left.value, root.right.value, node.value)
+        print left
+        print right
+        
+        if left['found']:
+            return {'path': [root.value] + left['path'],
+                    'found': True
+                }
+        elif right['found']:
+            return {'path': [root.value] + right['path'],
+                    'found': True
+                }
+ 
+ 
+print get_path(oneNode, fourNode_right_left_left)
+    
+def gather_nodes(root):
+    if root is None:
+        return []
+    else:
+        return [root.value] + gather_nodes(root.left) + gather_nodes(root.right)
+
+def calculate_time_to_burn(root, fireLeaf):
+    t = 0
+    
+    notVisitedNodes = gather_nodes(root)
+    latest_fire_nodes = [fireLeaf]
+    while len(notVisitedNodes) > 0:
+        
+        # step
+        for f_node in latest_fire_nodes:
+            notVisitedNodes.remove(f_node.value)
+
+            # add parent, childrent to latest_fire_nodes
+            f_node_path = get_path(root, f_node)
+            
+            
+        t += 1
+    
+    return t
+    
+
+#calculate_time_to_burn(oneNode, threeNode_right_right)
+    
---
+++ tree/bfs_dfs.py
@@ -0,0 +1,53 @@
+'''
+Created on Jun 28, 2018
+
+@author: eugen
+
+'''
+
+# A binary tree node
+class Node:
+     
+    # Constructor to create a new node
+    def __init__(self, data):
+        self.data = data 
+        self.left = None
+        self.right = None
+        
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def setRoot(self, val):
+        self.root = Node(val)
+
+    def insert(self, val):
+        if(self.root is None):
+            self.setRoot(val)
+        else:
+            self.insertNode(self.root, val)
+
+    def insertNode(self, currentNode, val):
+        if(val <= currentNode.data):
+            if(currentNode.left):
+                self.insertNode(currentNode.left, val)
+            else:
+                currentNode.leftChild = Node(val)
+        elif(val > currentNode.data):
+            if(currentNode.right):
+                self.insertNode(currentNode.right, val)
+            else:
+                currentNode.right = Node(val)
+
+    def find(self, val):
+        return self.findNode(self.root, val)
+
+    def findNode(self, currentNode, val):
+        if currentNode is None:
+            return False
+        elif val == currentNode.val:
+            return True
+        elif val < currentNode.val:
+            return self.findNode(currentNode.leftChild, val)
+        else:
+            return self.findNode(currentNode.rightChild, val)
