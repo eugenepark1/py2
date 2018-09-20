f3358ef21894e4ea8af46313c5961ce0bcc92009
eugenepark1 <eugene@everydayrelay.com>
2018-09-19 22:56:15 -0400

    max_product_k

--- interview/amzn/max_products_of_k.py
+++ interview/amzn/max_products_of_k.py
@@ -3,3 +3,17 @@ Created on Sep 18, 2018
 
 @author: eugenep
 '''
+
+
+def calculate_max_product_k(arr, k):
+    max_products_k = []
+    
+    arr = sorted(arr)
+    
+    print arr
+    return
+
+
+myArr = [-10,-8,-5,1,2,3,9]
+myK = 3
+print calculate_max_product_k(myArr, myK)
