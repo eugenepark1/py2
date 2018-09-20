2875ac6fe8d777225ead0458e77098b3452c4e06
eugenepark1 <eugene@everydayrelay.com>
2018-09-20 01:02:21 -0400

    max product k

--- interview/amzn/max_products_of_k.py
+++ interview/amzn/max_products_of_k.py
@@ -7,13 +7,28 @@ Created on Sep 18, 2018
 
 def calculate_max_product_k(arr, k):
     max_products_k = []
-    
     arr = sorted(arr)
+    i,j = 0, len(arr)-1
     
-    print arr
-    return
+    while len(max_products_k) < k:
+        left_product = arr[i] * arr[i+1]
+        right_product = arr[j] * arr[j-1]
+        
+        if k-len(max_products_k) == 1:
+            if arr[i] > arr[j]:
+                max_products_k.append(arr[i])
+            else:
+                max_products_k.append(arr[j])
+        else:
+            if left_product > right_product:
+                max_products_k += [arr[i], arr[i+1]]
+                i += 2
+            else:
+                max_products_k += [arr[j-1], arr[j]]
+                j -= 2
+    return max_products_k
 
 
 myArr = [-10,-8,-5,1,2,3,9]
-myK = 3
+myK = 4
 print calculate_max_product_k(myArr, myK)
