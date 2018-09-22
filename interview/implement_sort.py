d72e11cd329a1c7a022d42aa5fe38144b3f8fa89
eugenepark1 <eugene@everydayrelay.com>
2018-09-22 17:47:14 -0400

    implement sort

--- interview/implement_sort.py
+++ interview/implement_sort.py
@@ -2,23 +2,54 @@
 Created on Sep 21, 2018
 
 @author: eugene
+
+insertion sort O(n^2)
+selection sorts
+Merge sorts
+Bubble sorts
 '''
 
 def insertion_sort(arr):
-    ''' i: to track cur_num to move left to
-        j: to track number being compared by arr[i]
+    ''' 
+    while not all sorted
+        pick an element from unsorted (right-side)
+        place the element in sorted sublist (left-side)
+        
+        
+        |-sorted-|-unsorted-|
+        i: first element in unsorted
+        j: tmp ptr to move arr[i] to in sorted sublist
+        
+        0  1  2
+        2  5  1
     '''
-    for i in range(len(arr)):
-        j = i-1
-        #print "%s %s %s %s" % (i, j, arr[j], arr[i])
-        while  (j>0 and arr[j-1] < arr[j]):
-            tmp = arr[j]
-            arr[j] = arr[j-1]
-            arr[j-1] = tmp
-            j -= 1
+    def insert(arr, i, j):
+        '''insert arr[i] into jth spot
+        '''
+        tmp = arr[i]
+        # shift items from jth by one to right for until i-1th item
+        k = i
+        while k >=j:
+            arr[k] = arr[k-1]
+            k -=1
             
-            print "%s %s %s" % (arr,i,j)
+        arr[j] = tmp
+
+    i = 0
+    while i < len(arr):
+        j = i-1
+        do_swap = False
+        while  (j > 0 and arr[i] < arr[j]):
+            # find the spot in left-side
+            j -= 1
+            do_swap = True
+            #print "%s %s %s" % (arr,i,j)
+        
+        if do_swap:
+            print "insert(%s, %s, %s)" % (arr,i,j)
+            insert(arr, i , j)
+        i += 1
     
     return arr
 
-print insertion_sort([2,5,1])
+print insertion_sort([2,5,1,7,12,4,9,15,22])
