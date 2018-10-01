300c85e0af1dc253c3b310b968c6b8678368f0da
eugenepark1 <eugene@everydayrelay.com>
2018-10-01 03:19:48 -0400

    lc

---
+++ interview/leetcode/Longest_substring_with_k_repeating_chars.py
@@ -0,0 +1,29 @@
+'''
+Created on Oct 1, 2018
+
+@author: eugene
+
+Input:
+s = "aaabb", k = 3
+
+Output:
+3
+
+The longest substring is "aaa", as 'a' is repeated 3 times.
+
+Input:
+s = "ababbc", k = 2
+
+Output:
+5
+
+The longest substring is "ababb", as 'a' is repeated 2 times and 'b' is repeated 3 times.
+'''
+
+
+def longestSubstring(self, s, k):
+    """
+    :type s: str
+    :type k: int
+    :rtype: int
+    """
