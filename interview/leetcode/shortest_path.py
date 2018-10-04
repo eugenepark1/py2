9fb4f20bed651f289ee24e537b5a9995d28d1e90
eugenepark1 <eugene@everydayrelay.com>
2018-10-04 17:01:05 -0400

    sp

---
+++ interview/leetcode/shortest_path.py
@@ -0,0 +1,9 @@
+'''
+Created on Oct 4, 2018
+
+@author: eugenep
+
+the shortest path problem can be defined for graphs whether undirected, directed or mixed
+https://stackoverflow.com/questions/29464252/adjacency-matrix-in-python
+https://en.wikipedia.org/wiki/Shortest_path_problem
+'''
